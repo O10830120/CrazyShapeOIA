be90baf367759d2a1a790eb20238939cd48ea0a1
Student <o1083012@gm.pu.edu.tw>
2021-05-31 12:10:12 +0800

    2021上機考v3_(韩婉晶)

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -12,6 +12,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
+
+    var Flag:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +25,8 @@ class MainActivity : AppCompatActivity() {
             .override(800, 600)
             .into(imgTitle)
 
+        RndShape()
+
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(p0: View?): Boolean {
                 intent = Intent(this@MainActivity, GameActivity::class.java)
@@ -29,5 +34,22 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+
+        imgNext.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                RndShape()
+                }
+            })
+        }
+
+    fun RndShape(){
+        Flag=(1..4).random()
+        when (Flag){
+            1->imgNext.setImageResource(R.drawable.circle)
+            2->imgNext.setImageResource(R.drawable.square)
+            3->imgNext.setImageResource(R.drawable.star)
+            4->imgNext.setImageResource(R.drawable.triangle)
+
     }
+}
 }
